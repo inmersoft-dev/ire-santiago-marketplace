e6bb6b59b1f4848e29897d3c16749d2f2d2443e3
Sito8943 <sito8943@gmail.com>
2022-12-03 08:42:33 -0500

    Back Button

--- src/components/BackButton/BackButton.jsx
+++ src/components/BackButton/BackButton.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 // @mui components
-import { Button } from "@mui/material";
+import { IconButton, Button } from "@mui/material";
 
 // @mui icons
 import ChevronLeft from "@mui/icons-material/ChevronLeft";
@@ -12,23 +12,30 @@ import ChevronLeft from "@mui/icons-material/ChevronLeft";
 import SitoContainer from "sito-container";
 
 const BackButton = (props) => {
-  const { to } = props;
+  const { to, flat } = props;
   return (
     <SitoContainer
       sx={{
         position: "fixed",
-        top: "10px",
-        left: "10px",
+        top: "5px",
+        left: "1px",
+        zIndex: 20,
       }}
     >
       <Link to={to}>
-        <Button
-          variant="contained"
-          color="primary"
-          sx={{ minWidth: 0, borderRadius: "100%", padding: "5px" }}
-        >
-          <ChevronLeft />
-        </Button>
+        {!flat ? (
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ minWidth: 0, borderRadius: "100%", padding: "5px" }}
+          >
+            <ChevronLeft />
+          </Button>
+        ) : (
+          <IconButton color="secondary">
+            <ChevronLeft />
+          </IconButton>
+        )}
       </Link>
     </SitoContainer>
   );
@@ -36,10 +43,12 @@ const BackButton = (props) => {
 
 BackButton.defaultProps = {
   to: "/",
+  flat: false,
 };
 
 BackButton.propTypes = {
   to: PropTypes.string,
+  flat: PropTypes.bool,
 };
 
 export default BackButton;
